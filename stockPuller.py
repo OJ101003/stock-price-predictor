10d05edb727fa7a17e056b1bbe98797a7f633d62
OJ101003 <73129639+OJ101003@users.noreply.github.com>
2023-08-05 15:43:08 -0700

    Update stockPuller.py

--- stockPuller.py
+++ stockPuller.py
@@ -1,20 +1,19 @@
 import pandas as pd
 class stockPuller:
-    def stockChecker(url = None):
-        if url:
-            training_data = pd.read_csv(url)
+    def main_puller(ticker):
         while True:
             try:
+                training_data, stockTicker = stockPuller.history(ticker)
                 training_data['timestamp'] # This checks to see if the dataframe contains a timestamp column, which makes sure the training_data is valid data.
             except KeyError:
-                return False
+                ticker = input("Sorry, the ticker you have inputted might be incorrect, please type a new one: ")
             except Exception as error:
                 print("Sorry, an error has occurred:",error)
                 exit()
             else:
-                return True
+                return training_data, stockTicker
     def history(ticker):
-        apiKey = "Enter your API key here" 
+        apiKey = "Enter your API key here"
         stockTicker = ticker.upper()
         url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={stockTicker}&outputsize=full&apikey={apiKey}&datatype=csv'
         training_data = pd.read_csv(url) # Converts the data into a pandas dataframe
